8acdd83a38a169cba9c65f1e7864841f6b51d843
Yaroslav Vasilev <ia.vasilev@petrovich.tech>
2023-06-02 17:27:26 +0300

    Перехват исключения

--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,22 @@
+from .Pages.Main_page import MainPage
 import time
 
 from selenium.webdriver.common.by import By
 
 
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    time.sleep(3)
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
