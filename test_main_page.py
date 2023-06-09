45e9e5d3c90168a9368e0738fc9ea8ca76483de4
Yaroslav Vasilev <ia.vasilev@petrovich.tech>
2023-06-09 16:12:21 +0300

    Переход между страницами

--- test_main_page.py
+++ test_main_page.py
@@ -10,18 +10,16 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
-
-def test_guest_should_see_login_link(browser):
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
 
 def test_guest_should_see_login_page(browser):
     page = LoginPage(browser, link)
     page.open()
     go_to_login_page(browser)
     page.should_be_login_page()
+
