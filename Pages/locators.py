6a3e41c995a7fbb50142d9f6106b5b166a3ab290
Yaroslav Vasilev <ia.vasilev@petrovich.tech>
2023-06-13 10:42:48 +0300

    Независимость от данных

--- Pages/locators.py
+++ Pages/locators.py
@@ -15,3 +15,16 @@ class LoginPageLocators():
     REG_PASS2_FIELD = (By.CSS_SELECTOR, "#id_registration-password2")
     REG_BTN = (By.CSS_SELECTOR, "#register_form > button")
 
+class ProductPageLocators():
+#Кнопка добавить в корзину
+    BTN_ADD_TO_BASKET = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+#Alert о добавлении товара в корзину
+    MESSAGE_ABOUT_ADDING = (By.CSS_SELECTOR, "div.alertinner ")
+#Название товара
+    PRODUCT_TITTLE = (By.CSS_SELECTOR, "div.product_main h1")
+#Сумма в корзине
+    MESSAGE_BASKET_SUM = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
+#Цена товара
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+
+
--- test_main_page.py
+++ test_main_page.py
@@ -6,6 +6,7 @@ from .Pages.locators import LoginPageLocators
 from selenium.webdriver.common.by import By
 
 link = "http://selenium1py.pythonanywhere.com/"
+
 def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
